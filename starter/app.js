c64e844da01371410cd6f8add8eab5a3d381d862
whatthefoobar <irina.mntn@gmail.com>
2022-06-27 12:12:44 +0200

    setup mongoDB db and env file

--- starter/app.js
+++ starter/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
+app.use(express.static('./public'));
+app.use(express.json()); // to access req.body
 // routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App');
@@ -15,4 +19,14 @@ app.use('/api/v1/tasks', tasks);
 //app.patch('/api/v1/tasks/:id') - update a task
 //app.patch('/api/v1/tasks/:id') - update a task
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send('created a task');
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send('get single task');
+  res.json({ id: req.params.id });
 };
 
 const deleteTask = (req, res) => {
