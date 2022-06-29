0fc1dfeb71a5c16773f5ae99e3a1e92507e862d5
whatthefoobar <irina.mntn@gmail.com>
2022-06-29 10:37:16 +0200

    trycatch for each controller f as follows

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,11 +1,17 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
   res.send('all tasks from the file');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
 };
