5f2d836c96dff146c19a5a645a62d13bf86e81bb
whatthefoobar <irina.mntn@gmail.com>
2022-07-03 10:14:42 +0200

    test query params

--- 2. Store Api/starter/controllers/products.js
+++ 2. Store Api/starter/controllers/products.js
@@ -6,7 +6,9 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  console.log(req.query);
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
