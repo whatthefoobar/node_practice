336ff078edc59de7a2c39e22cef2ab47869ac0f2
whatthefoobar <irina.mntn@gmail.com>
2022-07-05 09:15:54 +0200

    added nane filter

--- 2. Store Api/starter/controllers/products.js
+++ 2. Store Api/starter/controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json({ products });
+  const search = 'ab'; // products that have at least an ab in the name
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   //this way if any query params are passed that are not featured in our object we get sent back the whole object unfiltered
   const queryObject = {};
 
@@ -15,6 +18,9 @@ const getAllProducts = async (req, res) => {
   }
   if (company) {
     queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
   console.log(queryObject);
