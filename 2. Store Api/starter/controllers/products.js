36b53cc31f2528206acc714407be4a92c7969b9d
whatthefoobar <irina.mntn@gmail.com>
2022-07-05 10:10:38 +0200

    sort f done

--- 2. Store Api/starter/controllers/products.js
+++ 2. Store Api/starter/controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'; // products that have at least an ab in the name
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   //this way if any query params are passed that are not featured in our object we get sent back the whole object unfiltered
   const queryObject = {};
 
@@ -23,8 +20,18 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  //console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    // products = products.sort();
+    console.log(sort);
+    const sortList = sort.split(',').join(' '); // to have all our sort terms added with space in between like so .sort('-name price');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
