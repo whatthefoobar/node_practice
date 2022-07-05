d44bd802630191ec2fc4c26cfee56e0a1750579a
whatthefoobar <irina.mntn@gmail.com>
2022-07-05 17:30:14 +0200

    added select fields filter

--- 2. Store Api/starter/controllers/products.js
+++ 2. Store Api/starter/controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price'); // only shows these 2 fields in our response
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   //this way if any query params are passed that are not featured in our object we get sent back the whole object unfiltered
   const queryObject = {};
 
@@ -27,6 +27,13 @@ const getAllProducts = async (req, res) => {
     console.log(sort);
     const sortList = sort.split(',').join(' '); // to have all our sort terms added with space in between like so .sort('-name price');
     result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  //fields only shows selected fields from the response object
+  if (fields) {
+    const fieldsList = fields.split(',').join(' '); // to have all our sort terms added with space in between like so .sort('-name price');
+    result = result.select(fieldsList);
   } else {
     result = result.sort('createdAt');
   }
