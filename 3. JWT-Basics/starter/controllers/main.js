22b8a50cbd59a989015b1d234d3dfd52658e3c38
whatthefoobar <irina.mntn@gmail.com>
2022-07-20 12:33:52 +0200

    done
    done

--- 3. JWT-Basics/starter/controllers/main.js
+++ 3. JWT-Basics/starter/controllers/main.js
@@ -4,13 +4,14 @@
 //setup authentication so only the request with JWT can access the dashboard
 
 const jwt = require('jsonwebtoken');
-const CustomApiError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors/custom-error');
+// const CustomApiError = require('../errors/custom-error');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new CustomApiError('Please provide username and password', 400);
+    throw new BadRequestError('Please provide username and password', 400);
   }
   // just for demo, normally provided by mongo db
   const id = new Date().getDate();
@@ -23,26 +24,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  const luckyNo = Math.floor(Math.random() * 100);
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomApiError('No token provided', 401);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //console.log(decoded); //{ id: 6, username: 'john', iat: 1657120224, exp: 1659712224 }
-
-    const luckyNo = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNo}`,
-    });
-  } catch (error) {
-    throw new CustomApiError('Not authorized to access this route', 400);
-  }
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNo}`,
+  });
 };
 
 module.exports = {
